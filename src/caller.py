22b45412a8fc78bf5f6fc1fd48959e8d9a91b8cc
elfsong <dumingzhe@126.com>
2024-05-27 17:42:57 +0800

    Add framework files

---
+++ src/caller.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+"""
+Author: Du Mingzhe (mingzhe@nus.edu.sg)
+Date: 27/05/2024
+Description: A collection of caller for Huggingface / OpenAI
+"""
+
+
+
+# Huggingface Transformers
+import transformers
+import torch
+
+model_id = "meta-llama/Meta-Llama-3-8B"
+
+pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto")
+pipeline("Hey how are you doing today?")
